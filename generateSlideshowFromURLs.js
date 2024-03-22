75416c4f54ff23197de811bfc04e9bc23d9d2194
Jmail <gamelelkazzaz@gmail.com>
2024-03-22 17:33:34 +0300

    new ffmpeg-fluent

--- generateSlideshowFromURLs.js
+++ generateSlideshowFromURLs.js
@@ -1,50 +1,36 @@
 const axios = require('axios');
 const fs = require('fs');
-const { promisify } = require('util');
 const ffmpeg = require('fluent-ffmpeg');
-const rimraf = require('rimraf');
-const mkdirp = require('mkdirp');
-
-const writeFileAsync = promisify(fs.writeFile);
-const rimrafAsync = promisify(rimraf);
-const mkdirpAsync = promisify(mkdirp);
 
 module.exports = async function generateSlideshowFromURLs(imageURLs, outputFilePath) {
     const frameDir = 'frames';
 
-    await mkdirpAsync(frameDir);
+    if (!fs.existsSync(frameDir)) fs.mkdirSync(frameDir);
 
-    try {
-        // Скачиваем каждое изображение и сохраняем его как кадр видео
-        await Promise.all(imageURLs.map(async (url, index) => {
-            const response = await axios.get(url, { responseType: 'arraybuffer' });
-            const outputPath = `${frameDir}/frame${index + 1}.jpg`;
-            await writeFileAsync(outputPath, Buffer.from(response.data, 'binary'));
-        }));
-
-        // Создаем видео из кадров с помощью fluent-ffmpeg
-        await new Promise((resolve, reject) => {
-            ffmpeg()
-                .input(`${frameDir}/frame%d.jpg`)
-                .inputOptions(['-framerate 0.75']) // Частота кадров в видео (1 кадр в секунду)
-                .videoCodec('libx264') // Кодек для видео
-                .outputOptions(['-pix_fmt yuv420p', '-vf scale=trunc(iw/2)*2:trunc(ih/2)*2']) // Обязательное масштабирование для сохранения четного размера
-                .on('end', () => {
-                    resolve();
-                })
-                .on('error', (err) => {
-                    reject(err);
-                })
-                .save(outputFilePath);
-        });
-
-        // Удаляем временные файлы
-        await rimrafAsync(frameDir);
-
-        return outputFilePath;
-    } catch (error) {
-        // Удаляем временные файлы в случае ошибки
-        await rimrafAsync(frameDir);
-        throw error;
+    // Download each image and save it as a video frame
+    for (let i = 0; i < imageURLs.length; i++)
+    {
+        const response = await axios.get(imageURLs[i], { responseType: 'arraybuffer' });
+        const outputPath = `${frameDir}/frame${i + 1}.jpg`;
+        fs.writeFileSync(outputPath, Buffer.from(response.data, 'binary'));
     }
-};
+    return await new Promise(resolve => {
+        ffmpeg()
+            .input('frames/frame%d.jpg') // Обратите внимание на %d вместо *
+            .inputOptions('-framerate 0.5')
+            .videoCodec('libx264')
+            .outputOptions('-r 30')
+            .outputOptions('-pix_fmt yuv420p')
+            .output(outputFilePath)
+            .on('end', () => {
+                fs.readdirSync(frameDir).forEach(file => fs.unlinkSync(`${frameDir}/${file}`));
+                fs.rmdirSync(frameDir);
+                console.log('Кодирование завершено')
+                return resolve()
+            })
+            .on('error', (err) => console.error('Ошибка при кодировании:', err))
+            .run();
+    })
+    
+}
+
